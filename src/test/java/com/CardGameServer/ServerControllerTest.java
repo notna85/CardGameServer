245ebfdb01ac33f99d7a64d197ea8a107eb78733
Anton John Erik Sundström <sundstrom85@gmail.com>
2020-12-08 09:43:17 +0100

    testclass

---
+++ src/test/java/com/CardGameServer/ServerControllerTest.java
@@ -0,0 +1,20 @@
+package com.CardGameServer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerControllerTest {
+
+    @Test
+    void open() {
+    }
+
+    @Test
+    void close() {
+    }
+
+    @Test
+    void handleMessage() {
+    }
+}
