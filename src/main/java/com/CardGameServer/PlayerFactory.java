8823c96fced988da58fe713452aa64558bd41fc1
Partast1 <andersa1989@gmail.com>
2020-12-08 14:55:02 +0100

    pogo & factories
    Encapsulation on pogo classes(Room, Player)
    finished factories.

--- src/main/java/com/CardGameServer/PlayerFactory.java
+++ src/main/java/com/CardGameServer/PlayerFactory.java
@@ -1,8 +1,9 @@
 package com.CardGameServer;
 
 public class PlayerFactory {
-    public Player createPlayer(){
-        return new Player();
+
+    public Player createPlayer(String name, String id){
+        return new Player(name, id);
 
 
     }
